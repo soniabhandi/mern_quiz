8a08d6943ed4a68c8eef5b1c2f1f0222bbe2ebbe
Sonia bhandi <soniabhandi@gmail.com>
2024-06-05 02:25:51 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyparser.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',  // allow only this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
